8c9f82097bf76fdcdf0ef5792e27e2126a9968e1
Junfusky <70178891+Junfusky@users.noreply.github.com>
2020-12-02 19:20:41 -0500

    Add files via upload

---
+++ JS/adoption.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(ev){
+    var cancel = myTool.$('cancel');
+
+
+    cancel.addEventListener('click', function(ev1) {
+        window.location.href="../html/interested.html";
+    });
+})
