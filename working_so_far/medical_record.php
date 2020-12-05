74a4271c7047d874ab8dd686354ea21351bb07bb
Ayaria <chenmatthewz30@gmail.com>
2020-12-04 22:10:54 -0500

    Linking animal_id and records

---
+++ working_so_far/medical_record.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medical records</title>
+    <link rel="stylesheet" href="../CSS/medical_record.css">
+</head>
+<body>
+    <div id="container">
+        <div id="title">Medical records for </div>
+        <div id="content">
+            <div class="back"><a href="javascript:history.go(-1)" style="color: black;"><span></span>Back</a></div>
+            <p>
+                <?php
+                include("functions.php");
+                function getMedicalHistory($animalID){
+                    $sql = "SELECT date, description FROM event_log WHERE event_type='medical' AND animal_id=" . $animalID . ";";
+                    return constructQuery($sql);
+                }
+                $q = getMedicalHistory($_GET["animal_id"]);
+                ?>
+                <table border=1 cellspacing=5 cellpadding=5>
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($row = $q->fetch()): ?>
+                            <tr>
+                                <td><?php echo htmlspecialchars($row['date']) ?></td>
+                                <td><?php echo htmlspecialchars($row['description']); ?></td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                </table>
+            </p>
+        </div>
+    </div>
+    <script src="../JS/myTools.js"></script>
+    <script src="../JS/medical_record.js"></script>
+</body>
+</html>
