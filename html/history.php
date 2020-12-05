a07a51f60303d18d1740275eb9e185d811933a2b
Ayaria <chenmatthewz30@gmail.com>
2020-12-04 21:05:06 -0500

    created medical_record and history.php

--- html/history.php
+++ html/history.php
@@ -3,22 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Medical records</title>
-    <link rel="stylesheet" href="../CSS/medical_record.css">
+    <title>History</title>
+    <link rel="stylesheet" href="../CSS/history.css">
 </head>
 <body>
     <div id="container">
-        <div id="title">Medical records for </div>
+        <div id="title">History records for </div>
         <div id="content">
             <div class="back"><a href="javascript:history.go(-1)" style="color: black;"><span></span>Back</a></div>
             <p>
             <?php
             include("../functions.php");
-            function getMedicalHistory($animalID){
-                $sql = "SELECT date, description FROM event_log WHERE event_type='medical' AND animal_id=" . $animalID . ";";
+            function getLocationHistory($animalID){
+                $sql = "SELECT date, address, city, state, zip_code FROM event_log e, location l WHERE (event_type='description' or event_type='adoption' or event_type='fostered') and e.location_id=l.location_id and animal_id=" . $animalID . ";";
                 return constructQuery($sql);
             }
-            $q = getMedicalHistory(4);
+            $q = getLocationHistory(4);
             while ($row = $q->fetch()):
 	            echo implode(" | ", $row)."<br>";
             endwhile;
@@ -27,6 +27,6 @@
         </div>
     </div>
     <script src="../JS/myTools.js"></script>
-    <script src="../JS/medical_record.js"></script>
+    <script src="../JS/history.js"></script>
 </body>
 </html>
