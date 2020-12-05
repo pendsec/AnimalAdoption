505d7cdaeaa037ad27852852da9d80eb3ead4989
Ayaria <chenmatthewz30@gmail.com>
2020-12-05 16:56:27 -0500

    adding css back in

--- working_so_far/history.php
+++ working_so_far/history.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>History</title>
-    <link rel="stylesheet" href="../CSS/history.css">
+    <link rel="stylesheet" href="../CSS/history.css?v=<?php echo time(); ?>">
 </head>
 <body>
     <div id="container">
